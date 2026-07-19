a04a029d69ae121a91093f068fb151ff7f8e4109
Hossain Khan <hello@hossain.dev>
2026-07-19 07:26:37 -0400

    Migrate screens to implement ParcelableScreen for Circuit 0.35.1 compatibility

--- app/src/main/java/dev/hossain/codematex/circuit/HomeScreen.kt
+++ app/src/main/java/dev/hossain/codematex/circuit/HomeScreen.kt
@@ -3,13 +3,13 @@ package dev.hossain.codematex.circuit
 import android.os.Parcelable
 import com.slack.circuit.runtime.CircuitUiEvent
 import com.slack.circuit.runtime.CircuitUiState
-import com.slack.circuit.runtime.screen.Screen
+import com.slack.circuit.runtime.screen.ParcelableScreen
 import dev.hossain.codematex.data.model.ChatSession
 import dev.hossain.codematex.data.model.CodingTopic
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data object HomeScreen : Screen {
+data object HomeScreen : ParcelableScreen {
     @kotlinx.serialization.Serializable
     sealed interface State : CircuitUiState {
         data object Loading : State
