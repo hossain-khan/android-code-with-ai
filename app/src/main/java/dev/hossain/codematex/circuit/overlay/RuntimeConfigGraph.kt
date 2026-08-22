844ade3ac1decc1aa6097d72d9900f970f889b63
Hossain Khan <hello@hossain.dev>
2026-08-22 09:27:25 -0400

    Remove dead RuntimeConfigGraph interface

--- app/src/main/java/dev/hossain/codematex/circuit/overlay/RuntimeConfigGraph.kt
+++
@@ -1,7 +0,0 @@
-package dev.hossain.codematex.circuit.overlay
-
-import dev.zacsweers.metro.AppScope
-import dev.zacsweers.metro.ContributesTo
-
-@ContributesTo(AppScope::class)
-interface RuntimeConfigGraph
