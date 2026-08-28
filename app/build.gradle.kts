8cbab0a1b0a78ea515defd86fa1d3877dcbae7c7
Hossain Khan <hello@hossain.dev>
2026-08-28 08:36:40 -0400

    Bump version to 1.15.1 (versionCode 26) and add release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.hossain.codematex"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 25
-        versionName = "1.15.0"
+        versionCode = 26
+        versionName = "1.15.1"
 
         // Read key or other properties from local.properties
         val localProperties =
