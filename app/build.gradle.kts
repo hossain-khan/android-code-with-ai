9df503d2da947db329c3f0afb0304e4eb6d4ac77
Hossain Khan <hello@hossain.dev>
2026-09-24 11:25:28 -0400

    Bump version to 1.24.0 (versionCode 48) with release notes

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "dev.hossain.codematex"
         minSdk = libs.versions.minSdk.get().toInt()
         targetSdk = libs.versions.targetSdk.get().toInt()
-        versionCode = 47
-        versionName = "1.23.1"
+        versionCode = 48
+        versionName = "1.24.0"
 
         // Read key or other properties from local.properties
         val localProperties =
